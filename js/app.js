102a14a88050c106cc20392eb31033febf8f02f7
manaki-e <48690114s@gmail.com>
2022-02-28 10:41:55 +0900

    全ての要素を動かすことが可能となった

--- js/app.js
+++ js/app.js
@@ -1,35 +1,68 @@
+/* =========================================
+jQueryを使って作成しようとした
+
+
 $(function () {
   $("li").mousedown(function () {
-    draggable(this);
-    
-    let startY =this.clientY;
-    let startX =this.clientX;
+    // draggable(this);
 
-    function draggable(target) {
-      target.onmousedown = function() {
-        document.onmousemove = mouseMove;
-      };
-      document.onmouseup = function() {
-        document.onmousemove = null;
-      };
-      function mouseMove(e) {
-        // console.log(e);
-        var event = e ? e : window.event;
-        // console.log(event);
-        target.style.top = event.clientY+ 'px';
-        target.style.left = event.clientX + 'px';
-      }
-    }
+    // let startY =this.clientY;
+    // let startX =this.clientX;
     
-  //   var place = this ? this : window.event;
-  //   $(this).css({"top": `${place}.clientY + px`, "left": `${place}.clientX + px`});
-  // });
+    $(this).mousemove(function (e) {
+      var event = e ? e : window.event;
+      this.style.top = event.clientY + 'px';
+      this.style.left = event.clientX + 'px';
+      
+      //   var place = this ? this : window.event;
+      //   $(this).css({"top": `${place}.clientY + px`, "left": `${place}.clientX + px`});
+      // });
 
-  // $(this).mouseup(function(e){
-  //   return;
+    });
   });
+
+  $(document).mouseup(function () {
+    $(document).mousemove() = null;
+  })
 });
 
+============================================ */
 
-// draggable(document.getElementById("1"));
-// draggable(document.getElementById("2"));
+/* ================================================
+idからliを取得して移動させる
+================================================= */
+
+draggable(document.getElementById("1"));
+draggable(document.getElementById("2"));
+draggable(document.getElementById("3"));
+draggable(document.getElementById("4"));
+draggable(document.getElementById("5"));
+draggable(document.getElementById("6"));
+draggable(document.getElementById("7"));
+draggable(document.getElementById("8"));
+draggable(document.getElementById("9"));
+draggable(document.getElementById("10"));
+draggable(document.getElementById("11"));
+draggable(document.getElementById("12"));
+draggable(document.getElementById("13"));
+draggable(document.getElementById("14"));
+draggable(document.getElementById("15"));
+
+function draggable(target) {
+
+  target.onmousedown = function () {
+
+    let startY = target.getBoundingClientRect().top;
+    let startX = target.getBoundingClientRect().left;
+
+    document.onmousemove = function (e) {
+      var event = e ? e : window.event;
+      target.style.top = event.clientY + 'px';
+      target.style.left = event.clientX - startX + 'px';
+    }
+  };
+};
+
+document.onmouseup = function () {
+  document.onmousemove = null;
+};
